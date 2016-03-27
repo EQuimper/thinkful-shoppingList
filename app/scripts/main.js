685da3647d62b8ded70de7a82e96b539c28a2a1e
UrNightmares <quimperemanuel@gmail.com>
2016-03-27 16:02:25 -0600

    add drag function etc...

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,34 @@
-console.log('\'Allo \'Allo!');
+/* global $: false */
+
+$(document).ready(function(){
+  'use strict';
+
+  $('form').submit(function(e){
+    if ($('input').val().length !== 0) { // Prevent item with 0
+      var value = $('input').val();
+      console.log(value);
+      $('#itemList').append('<li class="item ui-state-default">' + value + '</li>'); // Add to the list
+      $('form').find('input:text').val('');
+      e.preventDefault();
+    } else {
+      alert('You must add a item'); // Message cause of empty
+    }
+  });
+
+  // $('.itemList').draggable({
+  //
+  // });
+
+  $(function() {
+    $( "#itemList, #itemDone" ).sortable({
+      connectWith: ".connectedSortable"
+    }).disableSelection();
+  });
+
+  $('.item').draggable();
+
+  $('button').click(function() {
+    $('.completed > li').remove();
+  });
+
+});
